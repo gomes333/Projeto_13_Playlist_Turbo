cff456d65f97827d977153e8f3bff1f54e36b8d5
Isadora Gomes <isag4659@gmail.com>
2026-09-10 14:53:25 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -2,16 +2,19 @@ import streamlit as st
 import pandas as pd
 
 ### 1. Leia o Arquivo Dados_Artistas.csv e o Transforme em dataframe
+df = pd.read_parquet("Dados_Artistas.parquet")
 
+df.info()
 
 ### 2. Coloque um titulo na pagina
-
+st.title("isADORA_MÚSICA")
 
 ### 3. Coloque subtitulo titulo na pagina
-
-
+st.subheader("Seu artista,Sua vibe,Seu som.")
+''
 ### 4. Coloque uma logo na sidebar(barra lateral)
-
+st.image("logo2.png")
+st.sidebar.image("logo2.png")
 
 ### 5. Não mexa abaixo, estamos criando uma selectbox, para selecionar o artista
 artistas = st.sidebar.selectbox('Selecione o Artista', df['Artist'].unique())
